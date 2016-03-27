3842a790619a585966b4cbc1f5d7227bd5956df8
Bkliger <bkliger@comcast.net>
2016-03-26 19:26:55 -0700

    almost ready for submission

--- app.js
+++ app.js
@@ -1,30 +1,43 @@
 console.log("Sanity Check: JS is working!");
-var keyCountRacer1 = 0;
-var keyCountRacer2 = 0;
+var racer1HTML = "#firstRacer";
+var racer2HTML = "#secondRacer";
+var winningNumberOfMoves = 20;
 /* TODO figure out how to create a function that will create a Racer */
 
-
-// function Racer(){
-//   this.color =
-//
-// }
+/* Object Constructor */
+function Racer(key){
+  this.key = key;
+  this.counter = 0;
+  this.move = function(racer,movement) {
+    $(racer).animate({
+    "padding-left": "+=" + movement
+    }, 100);
+  };
+}
 
 $(document).ready(function() {
+/* create 2 racer objects */
+  var firstRacer = new Racer(65);
+    console.log(Racer.key);
+    var secondRacer = new Racer(76);
+  /* determine width of racetrack and calculate the length of a move */
   var width = $("#firstRaceTrack").width();
-  var movement = width/10-1;
-  console.log(movementString);
+  var movement = (width/winningNumberOfMoves)-1;
+
+  /* watch for keydown events*/
   $(window).on("keydown", function(event) {
-    if (event.which === 65) {
-      keyCountRacer1++;
-      console.log(keyCountRacer1);
-      $("#firstRacer").animate({
-        "padding-left": "+=" + movement
-      }, 100, function (){
-        if (keyCountRacer1 === 10) {
+    if (event.which === 65) {   //"a" is pressed
+      firstRacer.counter++;
+      firstRacer.move(racer1HTML,movement);
+      if (firstRacer.counter === winningNumberOfMoves) {
           alert("Racer 1 has won");
-        }
-      });
+      }
+    } else if (event.which === 76) { //"l" is pressed
+      secondRacer.counter++;
+      secondRacer.move(racer2HTML,movement);
+      if (secondRacer.counter === winningNumberOfMoves) {
+            alert("Racer 2 has won");
+      }
     }
   });
-
 });
---
+++ readme.md
@@ -0,0 +1,21 @@
+# Racing Game
+The goal of this game is to make 10 moves before the other player. Player 1 uses the "a" key and Player 2 uses the "l" key.
+
+
+# Technologies Used
+
+  * html
+  * bootstrap
+  * jquery
+
+# Existing Features
+
+  * Icons for players are pre-selected as are the keys to press.
+  * The moves are proportional to the size of the race course at the beginning of the race.
+  * reset button moves both players back to their starting points.
+
+# Planned Features
+
+  * Players select their icons and their keys
+
+# What changes would you make to your project if you continue to work on it? (bullet points)
